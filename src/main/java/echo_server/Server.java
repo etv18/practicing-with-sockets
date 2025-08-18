b92d5e8928d2a4667311144e8eaf808a1979667e
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-08-18 15:56:11 -0400

    main function to execute code

--- src/main/java/echo_server/Server.java
+++ src/main/java/echo_server/Server.java
@@ -67,4 +67,11 @@ public class Server {
         }
     }
 
+    public static void main(String[] args) throws IOException {
+        int port = 5000;
+        ServerSocket serverSocket = new ServerSocket(port);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+
 }
