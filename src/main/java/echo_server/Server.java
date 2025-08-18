409a18782f308d874fe9fce40eaf08d54c40cb79
Emmanuel Tavarez <tavareze2003@gmail.com>
2025-08-18 15:52:03 -0400

    Server class set up

---
+++ src/main/java/echo_server/Server.java
@@ -0,0 +1,18 @@
+package echo_server;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Server {
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private BufferedReader in;
+    private BufferedWriter out;
+
+    public Server(ServerSocket serverSocket){
+        this.serverSocket = serverSocket;
+    }
+}
